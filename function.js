1c80f0c7246142652d4f5ef00b94f100469cfe0f
msIg84 <igor@ampe.ru>
2013-08-16 09:10:58 -0700

    Create function.js
    Вывод полного списка факторионов.

---
+++ function.js
@@ -0,0 +1,9 @@
+function fact() {
+var factArr = [1, 2, 145, 40585]
+document.write("<ul>")
+for (i=0; i<4; i++) {
+li = "<li>Факторион = " +factArr[i]+ "</li>";
+document.write(li)
+}
+document.write("</ul>")
+  }
